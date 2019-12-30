e19cd9069a8ce8ac2499730cc5dcbab0bbe09970
AlexWuDDD <wkjworking@outlook.com>
2019-12-30 09:33:18 +0800

    2019-12-30

---
+++ cmake-build-debug/CMakeFiles/SmallVector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SmallVector.dir/SmallVector.cpp.o"
+  "SmallVector"
+  "SmallVector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SmallVector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Type_erasedAndCallableObjects.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Type_erasedAndCallableObjects.dir/Type_erasedAndCallableObjects.cpp.o"
+  "Type_erasedAndCallableObjects"
+  "Type_erasedAndCallableObjects.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Type_erasedAndCallableObjects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
